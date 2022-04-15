87d4f8958a8e1c7ef318bb31f973efcb548d148f
Matheus Sena <matheus_osena@hotmail.com>
2022-04-15 16:46:27 -0300

    Add Static Folder to the root and adding new views

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home
+from . import views
 
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', {"name": "Matheus"})
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', {'name': 'Matheus'})
