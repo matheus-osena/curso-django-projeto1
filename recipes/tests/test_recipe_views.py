71f6dc43c367fef8b3ec8f4db5137408bdf222d4
Matheus Sena <matheus_osena@hotmail.com>
2022-05-09 10:15:48 -0300

    Utilizando o @skip

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,4 +1,6 @@
 
+from unittest import skip
+
 from django.urls import resolve, reverse
 from recipes import views
 from recipes.tests.test_recipe_base import RecipeTestBase
@@ -20,6 +22,7 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
+    @skip("WIP")
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipe(self):
         response = self.client.get(reverse("recipes:home"))
         self.assertIn(
