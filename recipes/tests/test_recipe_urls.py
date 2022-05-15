d1ce1ef3e9b89e1ae241447dbb37194fcf45dd40
Matheus Sena <matheus_osena@hotmail.com>
2022-05-15 11:18:25 -0300

    Finalizando a view search com os testes

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -19,4 +19,3 @@ class RecipeURLsTest(TestCase):
     def test_recipe_search_url_is_correct(self):
         url = reverse('recipes:search')
         self.assertEqual(url, '/recipes/search/')
-# teste
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
@@ -30,7 +31,15 @@ def search(request):
     search_term = request.GET.get('q', '').strip()
     if not search_term:
         raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(Q(title__icontains=search_term) | Q(
+            description__icontains=search_term),
+          ), is_published=True,
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html',
                   context={
-                      'page_title': f'Search for "{search_term}" |'
+                      'page_title': f'Search for "{search_term}" |',
+                      'recipes': recipes,
                   })
