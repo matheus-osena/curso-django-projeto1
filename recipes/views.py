08a52b2cf514032ee8f652f13ab4522fbbe771ec
Matheus Sena <matheus_osena@hotmail.com>
2022-05-19 10:18:57 -0300

    Adicioanando a constante PER_PAGES

--- recipes/views.py
+++ recipes/views.py
@@ -7,11 +7,13 @@ from .models import Recipe
 
 # from utils.recipes.factory import make_recipe
 
+PER_PAGES = 9
+
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGES)
 
     return render(request, 'recipes/pages/home.html', context={"recipes": page_obj, 'pagination_range': pagination_range})  # noqa: E501
 
@@ -20,7 +22,7 @@ def category(request, category_id):
     recipes = get_list_or_404(Recipe.objects.filter(
         category__id=category_id, is_published=True).order_by('-id'))
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGES)
 
     return render(request,
                   'recipes/pages/category.html',
@@ -44,7 +46,7 @@ def search(request):
           ), is_published=True,
     ).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGES)
 
     return render(request, 'recipes/pages/search.html',
                   context={
